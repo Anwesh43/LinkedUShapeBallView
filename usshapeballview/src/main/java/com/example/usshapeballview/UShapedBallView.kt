5e49fdc55d7012a3cdeae2777fba4bb2bad2914c
Anwesh43 <anweshthecool0@gmail.com>
2021-05-26 00:47:55 +0530

    addng UShapedBallView class

--- usshapeballview/src/main/java/com/example/usshapeballview/UShapedBallView.kt
+++ usshapeballview/src/main/java/com/example/usshapeballview/UShapedBallView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawUSBNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawUShapedBall(scale, w, h, paint)
 }
+
+class UShapedBallView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
