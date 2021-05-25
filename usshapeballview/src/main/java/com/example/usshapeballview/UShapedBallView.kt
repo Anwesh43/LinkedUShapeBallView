ea5f4bdef7d14dc6e75bf8a285e5e5ab0e09aa5e
Anwesh43 <anweshthecool0@gmail.com>
2021-05-26 00:58:17 +0530

    adding USBNode class

--- usshapeballview/src/main/java/com/example/usshapeballview/UShapedBallView.kt
+++ usshapeballview/src/main/java/com/example/usshapeballview/UShapedBallView.kt
@@ -123,4 +123,45 @@ class UShapedBallView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class USBNode(var i : Int, val state : State = State()) {
+
+        private var next : USBNode? = null
+        private var prev : USBNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = USBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawUSBNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : USBNode {
+            var curr : USBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
