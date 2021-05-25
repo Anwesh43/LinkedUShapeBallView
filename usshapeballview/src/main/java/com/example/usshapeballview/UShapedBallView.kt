55daa53eb6a0ab267a1f1b40c9414fb562d6e97f
Anwesh43 <anweshthecool0@gmail.com>
2021-05-26 00:36:22 +0530

    adding Int and Float extension methods

--- usshapeballview/src/main/java/com/example/usshapeballview/UShapedBallView.kt
+++ usshapeballview/src/main/java/com/example/usshapeballview/UShapedBallView.kt
@@ -25,4 +25,9 @@ val rFactor : Float = 8.9f
 val sizeFactor : Float = 2.9f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
-val rot : Float = 90f
+val rot : Float = 180f
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
