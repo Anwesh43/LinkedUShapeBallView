9714c87c49ba0b25b3fd290f36339a26856269eb
Anwesh43 <anweshthecool0@gmail.com>
2021-05-26 00:46:43 +0530

    adding Canvas extension methods

--- usshapeballview/src/main/java/com/example/usshapeballview/UShapedBallView.kt
+++ usshapeballview/src/main/java/com/example/usshapeballview/UShapedBallView.kt
@@ -31,3 +31,32 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawUShapedBall(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    val sf4 : Float = sf.divideScale(3, parts)
+    val r : Float = Math.min(w, h) / rFactor
+    save()
+    translate(w / 2, h / 2)
+    save()
+    rotate(rot * sf3)
+    paint.style = Paint.Style.FILL
+    drawCircle(size - r, -r - h / 2 + (h / 2) * (sf2 + sf4), r, paint)
+    restore()
+    paint.style = Paint.Style.STROKE
+    drawArc(RectF(-size, -size, size, size), 0f, rot * sf1, false, paint)
+    restore()
+}
+
+fun Canvas.drawUSBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawUShapedBall(scale, w, h, paint)
+}
