33c1b26303875e6e9c2f6bfa2a4313eab4ca836d
Anwesh43 <anweshthecool0@gmail.com>
2021-05-26 00:34:39 +0530

    adding constants and libraries

--- usshapeballview/src/main/java/com/example/usshapeballview/UShapedBallView.kt
+++ usshapeballview/src/main/java/com/example/usshapeballview/UShapedBallView.kt
@@ -1,2 +1,28 @@
 package com.example.usshapeballview
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+import android.graphics.Canvas
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<Int> = arrayOf(
+    "#f44336",
+    "#9C27B0",
+    "#004D40",
+    "#FFAB00",
+    "#880E4F"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val rFactor : Float = 8.9f
+val sizeFactor : Float = 2.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val rot : Float = 90f
